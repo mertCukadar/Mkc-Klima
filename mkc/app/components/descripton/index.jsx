375a0d7fb9329b513b9056dda60e7c4cb75ceef7
cukadarMert <cukadar.mertkaan@gmail.com>
2024-05-06 13:19:30 +0300

    Description commit

--- mkc/app/components/descripton/index.jsx
+++ mkc/app/components/descripton/index.jsx
@@ -3,20 +3,28 @@ import React from "react";
 
 export function Description(){
     return(
-    <div className='flex flex-col justify-center'>
-        <div className='p-5 flex flex-row justify-center items-center'>
-            <div className='flex flex-col justify-center bg-cyan-900 rounded-lg p-5'>
-            <div className='flex'>
-                <h1 className=' text-4xl font-semibold mb-3'>MKC KLIMA</h1>
+    <div className='flex flex-col justify-center '>
+        <div className='p-5 flex flex-row justify-center items-center '>
+            <div className='flex flex-col justify-center bg-cyan-900 rounded-sm p-5 shadow-2xl shadow-slate-500'>
+            <div className=''>
+                <h1 className=' text-2xl font-semibold mb-3'>
+                    MKC Klima Elektrik Otomasyon
+                </h1>
+                <h1 className=' text-4xl font-semibold mb-3'>
+                Klima ve Otomasyonun Buluşması: İdeal Sıcaklık, Mükemmel Kontrol
+                </h1>
             </div>
 
             <div className='flex  flex-col justify-center mb-5'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex maxime fugiat aliquam minus cupiditate! Voluptate, debitis ducimus doloribus veritatis blanditiis unde rerum ea nesciunt obcaecati repudiandae adipisci iste tempora dolorum saepe soluta perferendis. Itaque natus saepe perspiciatis pariatur velit voluptatum doloremque, quaerat ex quas, amet expedita, beatae corporis dolor deserunt. Saepe, deleniti. Itaque provident aut, amet deleniti explicabo vitae reprehenderit repudiandae et ad vero nulla minima eos totam commodi error, veritatis ratione omnis cupiditate! Eaque magnam minima voluptates beatae magni porro, veniam reiciendis suscipit, commodi dolor neque nam aspernatur esse sapiente iste unde! Neque fuga incidunt sint temporibus, sequi laboriosam!</p>
+                <p>2008 yılında köklü bir deneyimle kurulan firmamız, klima ve havalandırma sistemleri alanında sektörün lideri olmaya devam ediyor. Ankara merkezli olarak başladığımız bu yolculukta, 2024 itibariyle Muğla Ortaca'da da şubemizle hizmetinizdeyiz. Müşterilerimize sağladığımız çözümlerdeki uzmanlığımız ve tutkumuzla, her zaman en üstün hizmeti sunma gayretindeyiz.
+
+Firmamız, klima ve havalandırma sistemleri konusundaki derin bilgi ve tecrübemizi müşterilerimizin taleplerini en iyi şekilde karşılamak için kullanıyor. Müşteri memnuniyeti bizim için en önemli önceliklerden biridir ve bu doğrultuda çalışmalarımızı sürdürüyoruz. Sektördeki yenilikleri yakından takip ediyor, teknoloji ve kaliteyi bir araya getirerek müşterilerimize en verimli ve güvenilir çözümleri sunuyoruz.
+                </p>
             </div>
 
             <div className="flex items-center">
                 <Link href="/contact">
-                    <p className=" text-white p-3 rounded-xl border-cyan-400 border-2 cursor-pointer hover:bg-cyan-400 hover:text-cyan-900">İletişim</p>
+                    <p className=" text-white p-3 rounded-sm border-cyan-800 border-2 cursor-pointer hover:border-cyan-700 hover:text-white">İletişim</p>
                 </Link>
             </div>
            
