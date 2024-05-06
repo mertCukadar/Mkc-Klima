10523d75a542748b4875bbd91cd04e85162b3d3a
cukadarMert <cukadar.mertkaan@gmail.com>
2024-05-06 13:17:10 +0300

    layout main page commit

--- mkc/app/layout.jsx
+++ mkc/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Header } from "./components/header";
+import { Footer } from "./components/footer";
 import { Navigation } from "./components/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
         
         {children}
         
-        <Header />
+        <Footer />
       </body>
     </html>
   );
