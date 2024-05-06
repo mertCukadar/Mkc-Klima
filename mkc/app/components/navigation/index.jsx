50707f24f739baf64a635e260fa74655f4b690e4
cukadarMert <cukadar.mertkaan@gmail.com>
2024-05-06 13:20:17 +0300

    Navigation

--- mkc/app/components/navigation/index.jsx
+++ mkc/app/components/navigation/index.jsx
@@ -1,25 +1,32 @@
+'use client'
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { MdMenu } from "react-icons/md";
 
 
 export function Navigation(){
+
     return(
-        <nav className="flex justify-between items-center  bg-cyan-950 shadow-lg p-5 text-white hover:bg-white hover:text-black">
-        <div className="border-black border-spacing-2 border-solid">
-        <Image src="/logo.jpeg" width={200} height={200} />
+        <nav className="flex justify-between items-center  bg-white shadow-lg p-5 text-black">
+       
+        <div className=" border-black border-spacing-2 border-solid ">
+        <Image src="/logo.jpeg" width={150} height={150} />
         </div>
         <div className="flex ">
-          <ul className="flex space-x-5">
-            <Link className="font-semibold"
+          <ul className="hidden md:flex space-x-5">
+            <Link className=" text-slate-700 px-4 py-2 rounded-full border-cyan-800 border-1 cursor-pointer hover:bg-cyan-800 hover:text-white"
                 href="/aboutUs">Hakkımızda</Link>
-            <Link className="font-semibold"
-                href="/solutions">Çözümlerimiz</Link>
+            <Link className=" text-slate-700 px-4 py-2 rounded-full border-cyan-800 border-1 cursor-pointer hover:bg-cyan-800 hover:text-white"
+                href="#solutions">Çözümlerimiz</Link>
           </ul>
         </div>
-        <div className="px-5 mx-5 bg-cyan-800 rounded-full h-10 flex align-middle items-center text-white hover:bg-slate-300 hover:text-black">
-          <Link href="/contact">İletişim</Link>
+        <div className="flex md:hidden">
+          <MdMenu className="text-4xl" />
+        </div>
+        <div className="hidden px-5 mx-5 bg-cyan-800 rounded-full h-10 md:flex align-middle items-center text-white hover:bg-slate-300 hover:text-black">
+          <a href="/contact" scroll={false}>İletişim</a>
         </div>
       </nav>
     )
-}
+  }
